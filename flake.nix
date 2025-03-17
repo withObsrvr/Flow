5aa0d8d942f2d5c48a01aadd34b6bf72c7190e0c
Tillman Mosley III <tmosleyiii@gmail.com>
2025-03-17 13:25:03 -0400

    Add flake.nix for Nix build system

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Stellar Flow Data Pipeline";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages = {
+          default = pkgs.buildGoModule {
+            pname = "flow";
+            version = "0.1.0";
+            src = ./.;
+            vendorSha256 = null; # Let Nix calculate this, then fill it in
+          };
+        };
+
+        devShell = pkgs.mkShell {
+          buildInputs = [ pkgs.go_1_23 ];
+        };
+      }
+    );
+}
