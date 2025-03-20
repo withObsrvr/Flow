eed3ef3ff7f32a91fbe09e7df7b47acb685a1524
Tillman Mosley III <tmosleyiii@gmail.com>
2025-03-20 14:05:36 -0400

    update goproxy

--- flake.nix
+++ flake.nix
@@ -19,9 +19,15 @@
             src = ./.;
             # Set vendorHash to null to disable vendoring and fetch dependencies from the network
             vendorHash = null;
-            # Ensure we're using -mod=mod to download modules directly from network
-            proxyVendor = true;
+            # Ensure proper Go module proxy configuration
+            proxyVendor = false; # Don't use the built-in proxy behavior
             flags = [ "-mod=mod" ];
+            # Set environment variables for go module downloads
+            env = {
+              GOPROXY = "https://proxy.golang.org,direct";
+              GO111MODULE = "on";
+              GOSUMDB = "sum.golang.org";
+            };
             # Specify the main packages to build
             subPackages = [ 
               "cmd/flow" 
@@ -35,6 +41,12 @@
           buildInputs = [ 
             pkgs.go_1_23
           ];
+          # Also set the Go proxy environment variables in the dev shell
+          shellHook = ''
+            export GOPROXY="https://proxy.golang.org,direct"
+            export GO111MODULE="on"
+            export GOSUMDB="sum.golang.org"
+          '';
         };
       }
     );
